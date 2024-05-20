03926030b835d6495c1312309685e587d5e220fe
etwither <etwitherspoon76@gmail.com>
2024-05-20 13:08:13 -0500

    will now create the dataset needed for the classifier

--- dataSet.py
+++ dataSet.py
@@ -1,26 +1,39 @@
 import os
+import pickle
 import cv2 as cv
 import mediapipe as mp
 import matplotlib.pyplot as plt
 
+#load the model for hand tracking
 mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
 hands = mp_hands.Hands(static_image_mode = True, min_detection_confidence = 0.3)
 
 DATA_DIR = "./data"
+data = []
+lables = []
 
 for dir in os.listdir(DATA_DIR):
     for img_dir in os.listdir(os.path.join(DATA_DIR,dir))[:1]:
+        data_aux = []
         img = cv.imread(os.path.join(DATA_DIR,dir,img_dir))
         img_rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
+        #detect hands in image
         result = hands.process(img_rgb)
+
+        #draw the landmarks
         if result.multi_hand_landmarks:
             for hand_landmark in result.multi_hand_landmarks:
-                #draw the landmarks
-                mp_draw.draw_landmarks(img_rgb, hand_landmark, mp_hands.HAND_CONNECTIONS)
+                for i in range(len(hand_landmark.landmark)):
+                    x = hand_landmark.landmark[i].x
+                    y = hand_landmark.landmark[i].y
+                    data_aux.append(x)
+                    data_aux.append(y)
 
-        plt.figure()
-        plt.imshow(img_rgb)
+            data.append(data_aux)
+            lables.append(dir)
 
-plt.show()
+f = open('data.pickle', 'wb')
+pickle.dump({'data':data, 'lables':lables}, f)
+f.close()
